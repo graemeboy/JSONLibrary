40c022a1f2c37998f3cc6926985beb03ae5287c7
Graeme Boy <graemeboy@gmail.com>
2014-04-18 14:02:27 -0500

    Add 2 more tests

--- src/DecoderTest.java
+++ src/DecoderTest.java
@@ -45,7 +45,7 @@ public class DecoderTest
     JSONDecoder decoder = new JSONDecoder (catsName);
     JSONObject obj = (JSONObject) decoder.jsonDecode ();
 
-    assertEquals ("testing false", null, obj.get ("cat's name").get ());
+    assertEquals ("testing null", null, obj.get ("cat's name").get ());
 
   }// testNull
 
@@ -58,7 +58,39 @@ public class DecoderTest
     JSONDecoder decoder = new JSONDecoder (idealScore);
     JSONObject obj = (JSONObject) decoder.jsonDecode ();
 
-    assertEquals ("testing false", 100,
+    assertEquals ("testing number", 100,
                   ((JSONNumber) obj.get ("ideal exam score")).getInteger ());
   }// testNumber
+
+  /*
+   * Added on Friday; no other code was changed.
+   */
+
+  @Test
+  public void
+    testString ()
+      throws Exception
+  {
+    String json = "{\"name\": \"Graeme\"}";
+    JSONDecoder decoder = new JSONDecoder (json);
+    JSONObject obj = (JSONObject) decoder.jsonDecode ();
+
+    assertEquals ("testing string", "Graeme", obj.get ("name").get ());
+  }// testString
+
+  @Test
+  public void
+    testNested ()
+      throws Exception
+  {
+    String scores = "{\"person\":{\"firstname\":\"Graeme\", \"age\": 23}}";
+    JSONDecoder decoder = new JSONDecoder (scores);
+    // This is messy, so we need to add a wrapper that will take care of this
+    // stuff. It ought to be one operation to get the correct data and type.
+    JSONObject obj = (JSONObject) decoder.jsonDecode ();
+    JSONObject person = (JSONObject) obj.get ("person");
+    int idealScore = ((JSONNumber) person.get ("age")).getInteger ();
+    assertEquals ("testing nested", 23, idealScore);
+  }// testNested
+
 }
